5778600a0bb3aee5a6fe6e3d5b9825f8e6c25924
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-04-18 10:45:06 +0530

    PixelPass JS changes (#6)
    * [INJIMOB-932] - use base45-web
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1184] - PixelPass JS CBOR decode support (#3)
    
    * [INJIMOB-1184] - add CBOR decode functionality
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1184] - indentation and styling of code
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    * [INJIMOB-1184] - update patch version
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- js/src/shared/Constants.js
+++ js/src/shared/Constants.js
@@ -2,7 +2,7 @@ import {ECC} from "../types/ECC";
 
 export const DEFAULT_ZLIB_COMPRESSION_LEVEL = 9
 
-export const DEFAULT_ECC_LEVEL = ECC.M
+export const DEFAULT_ECC_LEVEL = ECC.L
 export const COLOR_BLACK = "#000000"
 export const COLOR_WHITE = "#FFFFFF"
 export const DEFAULT_QR_SCALE = 10

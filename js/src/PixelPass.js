914364eb3a9831936b623f63dcae6f0cdd8e5cb8
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-04-18 10:25:46 +0530

    [INJIMOB-1184] - indentation and styling of code
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- js/src/PixelPass.js
+++ js/src/PixelPass.js
@@ -9,35 +9,37 @@ import {
 const b45 = require("base45-web");
 const pako = require('pako');
 import QRCode from 'qrcode';
+
 const cbor = require('cbor');
+
 export function decodeData(base45Data) {
     const decodedBase45Data = b45.decode(base45Data);
     const compressedData = pako.inflate(decodedBase45Data);
-    const textData =  new TextDecoder().decode(compressedData);
-    try{
+    const textData = new TextDecoder().decode(compressedData);
+    try {
         const decodedCBORData = cbor.decode(textData);
         if (decodedCBORData) return JSON.stringify(decodedCBORData)
         return textData
-    }catch (e){
+    } catch (e) {
         return textData
     }
 }
 
-export function generateQRData(data, header= "" ) {
-    const compressedData= pako.deflate(data , {level : DEFAULT_ZLIB_COMPRESSION_LEVEL})
-    return  header + b45.encode(compressedData).toString()
+export function generateQRData(data, header = "") {
+    const compressedData = pako.deflate(data, {level: DEFAULT_ZLIB_COMPRESSION_LEVEL})
+    return header + b45.encode(compressedData).toString()
 }
 
-export async function generateQRBase64Image(data, ecc = DEFAULT_ECC_LEVEL, header="" ) {
-    const base45Data = generateQRData(data,header)
+export async function generateQRBase64Image(data, ecc = DEFAULT_ECC_LEVEL, header = "") {
+    const base45Data = generateQRData(data, header)
     const opts = {
-        errorCorrectionLevel: ecc ,
+        errorCorrectionLevel: ecc,
         quality: DEFAULT_QR_QUALITY,
         margin: DEFAULT_QR_BORDER,
         scale: DEFAULT_QR_SCALE,
         color: {
-            dark:COLOR_BLACK,
-            light:COLOR_WHITE
+            dark: COLOR_BLACK,
+            light: COLOR_WHITE
         }
     }
     return QRCode.toDataURL(base45Data, opts);

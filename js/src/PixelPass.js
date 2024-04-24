e2c2c27e9a6148712c98cae1d9064902914f1842
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-04-24 18:04:37 +0530

    [INJIMOB-1225] - update JS api names and test accordingly
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- js/src/PixelPass.js
+++ js/src/PixelPass.js
@@ -5,32 +5,18 @@ import {
     DEFAULT_QR_QUALITY,
     DEFAULT_QR_SCALE, DEFAULT_ZLIB_COMPRESSION_LEVEL
 } from "./shared/Constants";
+import QRCode from 'qrcode';
 
 const b45 = require("base45-web");
 const pako = require('pako');
-import QRCode from 'qrcode';
-
 const cbor = require('cbor-web');
-
-export function decodeData(base45Data) {
-    const decodedBase45Data = b45.decode(base45Data);
-    const compressedData = pako.inflate(decodedBase45Data);
-    const textData = new TextDecoder().decode(compressedData);
-    try {
-        const decodedCBORData = cbor.decode(textData);
-        if (decodedCBORData) return JSON.stringify(decodedCBORData)
-        return textData
-    } catch (e) {
-        return textData
-    }
-}
 
 export function generateQRData(data, header = "") {
     const compressedData = pako.deflate(data, {level: DEFAULT_ZLIB_COMPRESSION_LEVEL})
     return header + b45.encode(compressedData).toString()
 }
 
-export async function generateQRBase64Image(data, ecc = DEFAULT_ECC_LEVEL, header = "") {
+export async function generateQRCode(data, ecc = DEFAULT_ECC_LEVEL, header = "") {
     const base45Data = generateQRData(data, header)
     const opts = {
         errorCorrectionLevel: ecc,
@@ -43,4 +29,17 @@ export async function generateQRBase64Image(data, ecc = DEFAULT_ECC_LEVEL, heade
         }
     }
     return QRCode.toDataURL(base45Data, opts);
+}
+
+export function decode(data) {
+    const decodedBase45Data = b45.decode(data);
+    const compressedData = pako.inflate(decodedBase45Data);
+    const textData = new TextDecoder().decode(compressedData);
+    try {
+        const decodedCBORData = cbor.decode(textData);
+        if (decodedCBORData) return JSON.stringify(decodedCBORData)
+        return textData
+    } catch (e) {
+        return textData
+    }
 }

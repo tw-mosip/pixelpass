f5abe8ab72c1280aaf4360ef0042b1a79467f713
Ayushi Dey <ayushirakshitdey@gmail.com>
2024-05-07 18:34:08 +0530

    INJIMOB-1302: upgrade java version to 20
    Signed-off-by: Ayushi Dey <ayushirakshitdey@gmail.com>

--- kotlin/PixelPass/build.gradle.kts
+++ kotlin/PixelPass/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.config.JvmTarget
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -20,11 +22,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "20"
     }
 }
 

7695a91d2b0c42f678f129c428bdc7eb8399c5a6
Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
2024-06-17 16:10:24 +0530

    [INJIMOB-1493] override generatepom task
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>

--- kotlin/PixelPass/build.gradle.kts
+++ kotlin/PixelPass/build.gradle.kts
@@ -68,3 +68,7 @@ tasks.register<Jar>("jarRelease") {
     destinationDirectory.set(layout.buildDirectory.dir("libs"))
 }
 apply(from = "publish-artifact.gradle")
+
+tasks.register("generatePom") {
+    dependsOn("generatePomFileForAarPublication", "generatePomFileForJarReleasePublication")
+}

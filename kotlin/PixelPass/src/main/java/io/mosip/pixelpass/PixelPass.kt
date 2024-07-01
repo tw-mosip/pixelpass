368bec91d07f88a0765f9c16ddf0475958f4c7b5
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-07-01 12:14:52 +0530

    [INJIMOB-1506] - json map cbor implementation for kotlin
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- kotlin/PixelPass/src/main/java/io/mosip/pixelpass/PixelPass.kt
+++ kotlin/PixelPass/src/main/java/io/mosip/pixelpass/PixelPass.kt
@@ -5,9 +5,11 @@ import android.graphics.Color
 import android.util.Log
 import co.nstant.`in`.cbor.CborDecoder
 import co.nstant.`in`.cbor.CborEncoder
+import co.nstant.`in`.cbor.model.UnicodeString
 import io.mosip.pixelpass.cbor.Utils
 import io.mosip.pixelpass.shared.QR_BORDER
 import io.mosip.pixelpass.shared.QR_SCALE
+import io.mosip.pixelpass.shared.decodeHex
 import io.mosip.pixelpass.types.ECC
 import io.mosip.pixelpass.zlib.ZLib
 import io.nayuki.qrcodegen.QrCode
@@ -69,6 +71,40 @@ class PixelPass {
          }
 
         return (header + b45EncodedData)
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun getMappedCborData(jsonData: JSONObject, mapper: Map<String,String>): String {
+
+        val mappedJson = JSONObject()
+        val iterator = jsonData.keys().iterator()
+        while (iterator.hasNext()){
+            val next = iterator.next()
+            val key = mapper[next] ?: next
+            val value = jsonData.get(next)
+            mappedJson.put(key,value)
+        }
+
+        val payload = Utils().toDataItem(mappedJson)
+
+        val baos = ByteArrayOutputStream()
+        CborEncoder(baos).encode(payload)
+        return baos.toByteArray().toHexString()
+    }
+
+    fun decodeMappedCborData(cborData: String, mapper: Map<String,String>): String {
+        val cborDecodedData = CborDecoder(ByteArrayInputStream(cborData.decodeHex())).decode()[0]
+        val jsonData =  (Utils().toJson(cborDecodedData) as JSONObject)
+
+        val payload = JSONObject()
+        val iterator = jsonData.keys().iterator()
+        while (iterator.hasNext()){
+            val next = iterator.next()
+            val key = mapper[next] ?: next
+            val value = jsonData.get(next)
+            payload.put(key,value)
+        }
+        return payload.toString()
     }
     private fun toBitmap(qrCode: QrCode): Bitmap {
         Objects.requireNonNull(qrCode)

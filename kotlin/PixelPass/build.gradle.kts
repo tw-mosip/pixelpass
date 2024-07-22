cfce6f18af0a549f57d9c89d4655a5a9703c93fa
Sreenadh S <32409698+sree96@users.noreply.github.com>
2024-07-22 12:43:32 +0530

    [INJIMOB-1634] - jar version update
    Signed-off-by: Sreenadh S <32409698+sree96@users.noreply.github.com>

--- kotlin/PixelPass/build.gradle.kts
+++ kotlin/PixelPass/build.gradle.kts
@@ -56,10 +56,10 @@ tasks.register<Jar>("jarRelease") {
     }
     manifest {
         attributes["Implementation-Title"] = project.name
-        attributes["Implementation-Version"] = "1.4-SNAPSHOT"
+        attributes["Implementation-Version"] = "1.5-SNAPSHOT"
     }
     archiveBaseName.set("${project.name}-release")
-    archiveVersion.set("1.4-SNAPSHOT")
+    archiveVersion.set("1.5-SNAPSHOT")
     destinationDirectory.set(layout.buildDirectory.dir("libs"))
 }
 apply(from = "publish-artifact.gradle")
